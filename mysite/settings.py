559c3623b977c4c58365bc0ba33497a58799e6a0
HosokiTakashi <takashihosoki@gmail.com>
2021-05-09 11:21:57 +0900

    manage.py修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = Path(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_ROOT = Path()
